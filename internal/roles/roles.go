9b194eb3ca3fe1ca6a3e235b0aa116fe14beda52
GGP1 <ggpalomeque@gmail.com>
2021-08-06 23:10:42 -0300

    Create roles package

---
+++ internal/roles/roles.go
@@ -0,0 +1,26 @@
+package roles
+
+import (
+	"github.com/GGP1/groove/internal/permissions"
+	"github.com/GGP1/groove/internal/romap"
+)
+
+const (
+	// Host is the default role used when creating an event
+	Host = "host"
+	// Attendant is the default role used when a user is attending an event
+	Attendant = "attendant"
+	// Moderator is in charge of banning problematic users
+	Moderator = "moderator"
+	// Viewer can see the content's of an event's page
+	Viewer = "viewer"
+)
+
+// Reserved is a read-only map that contains reserved roles
+// and its permission keys applying to all events.
+var Reserved = romap.New(map[string]interface{}{
+	Host:      []string{permissions.All},
+	Attendant: []string{permissions.Access},
+	Moderator: []string{permissions.Access, permissions.BanUsers},
+	Viewer:    []string{permissions.ViewEvent},
+})
