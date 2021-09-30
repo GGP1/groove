b1c78d6d2f229a8036404e0733d1c0eb80d1541f
GGP1 <ggpalomeque@gmail.com>
2021-09-30 20:30:48 -0300

    Implement sqltx package

---
+++ internal/sqltx/sqltx.go
@@ -0,0 +1,30 @@
+package sqltx
+
+import (
+	"context"
+	"database/sql"
+)
+
+// txKey is the context key for the sql transaction.
+var txKey key
+
+type key struct{}
+
+// NewContext returns a new context with a sql transaction in it.
+// The new context must replace the old one.
+//
+// This function should be called inside handlers (not services),
+// otherwise the context won't be updated properly as it's passed by value.
+func NewContext(ctx context.Context, tx *sql.Tx) context.Context {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	return context.WithValue(ctx, txKey, tx)
+}
+
+// FromContext returns the sql transaction stored in the context.
+//
+// It panics if there is no transaction.
+func FromContext(ctx context.Context) *sql.Tx {
+	return ctx.Value(txKey).(*sql.Tx)
+}
